6d3448dd6effb7f70e1cee0dd481863d907fc811
Aya Tokikaze <git@ayatk.com>
2017-09-11 15:08:21 +0900

    APK出力の仕様が変わってたので修正

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ ROOT_DIR=`git rev-parse --show-toplevel`
 VERSION=`grep "def VERSION" $ROOT_DIR/app/build.gradle | sed -E 's/.*"(.+)"$/\1/'`
 
 curl \
-  -F "file=@app/build/outputs/apk/biblio-v${VERSION}-nightly-release.apk" \
+  -F "file=@app/build/outputs/apk/nightly/release/biblio-v${VERSION}.apk" \
   -F "token=${DEPLOY_GATE_API_KEY}" \
   -F "message=Build #${CIRCLE_BUILD_NUM}" \
   -F "distribution_key=3f542ebaa365a1893d3467b34eb27c0a77ca67a7" \
