c4f1894cb49a682748076da50d7c14e3df5c76d5
Aya Tokikaze <git@ayatk.com>
2017-09-15 01:29:26 +0900

    SearchView関係をActivityに移した
    RxBusいらない子(あとで使う)

--- app/src/main/kotlin/com/ayatk/biblio/view/activity/SearchActivity.kt
+++ app/src/main/kotlin/com/ayatk/biblio/view/activity/SearchActivity.kt
@@ -9,17 +9,17 @@ import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.view.GravityCompat
+import android.support.v7.widget.SearchView
 import android.view.Menu
 import android.view.MenuItem
 import com.ayatk.biblio.R
 import com.ayatk.biblio.databinding.ActivitySearchBinding
-import com.ayatk.biblio.event.DrawerOpenEvent
-import com.ayatk.biblio.util.RxBus
 import com.ayatk.biblio.view.fragment.SearchFragment
 
 
 class SearchActivity : BaseActivity() {
 
+  private lateinit var searchView: SearchView
   private val binding: ActivitySearchBinding by lazy {
     DataBindingUtil.setContentView<ActivitySearchBinding>(this, R.layout.activity_search)
   }
@@ -32,15 +32,42 @@ class SearchActivity : BaseActivity() {
     replaceFragment(SearchFragment.newInstance(), R.id.content_view)
   }
 
-  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-    menuInflater.inflate(R.menu.activity_search, menu)
-    return super.onCreateOptionsMenu(menu)
+  override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    super.onCreateOptionsMenu(menu)
+    menuInflater.inflate(R.menu.menu_search, menu)
+
+    val menuItem = menu.findItem(R.id.action_search)
+    menuItem.setOnActionExpandListener(object : MenuItem.OnActionExpandListener {
+      override fun onMenuItemActionExpand(item: MenuItem): Boolean {
+        return true
+      }
+
+      override fun onMenuItemActionCollapse(item: MenuItem): Boolean {
+        onBackPressed()
+        return false
+      }
+    })
+    menuItem.expandActionView()
+
+    searchView = menuItem.actionView as SearchView
+    searchView.apply {
+      setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+        override fun onQueryTextSubmit(query: String): Boolean {
+          return onQueryTextChange(query)
+        }
+
+        override fun onQueryTextChange(newText: String): Boolean {
+          return true
+        }
+      })
+    }
+    return true
   }
 
   override fun onOptionsItemSelected(item: MenuItem): Boolean {
     if (item.itemId == R.id.action_filter) {
       binding.drawerLayout.openDrawer(GravityCompat.END)
-      RxBus.publish(DrawerOpenEvent())
+      searchView.clearFocus()
       return true
     }
     return super.onOptionsItemSelected(item)
--- app/src/main/kotlin/com/ayatk/biblio/view/fragment/SearchFragment.kt
+++ app/src/main/kotlin/com/ayatk/biblio/view/fragment/SearchFragment.kt
@@ -6,63 +6,19 @@ package com.ayatk.biblio.view.fragment
 
 import android.content.Context
 import android.os.Bundle
-import android.support.v7.widget.SearchView
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import com.ayatk.biblio.R
 import com.ayatk.biblio.databinding.FragmentSearchBinding
-import com.ayatk.biblio.event.DrawerOpenEvent
-import com.ayatk.biblio.util.RxBus
 
 class SearchFragment : BaseFragment() {
 
   lateinit var binding: FragmentSearchBinding
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setHasOptionsMenu(true)
-  }
-
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                             savedInstanceState: Bundle?): View? {
     binding = FragmentSearchBinding.inflate(inflater, container, false)
     return binding.root
-  }
-
-  override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-    super.onCreateOptionsMenu(menu, inflater)
-    inflater.inflate(R.menu.menu_search, menu)
-
-    val menuItem = menu.findItem(R.id.action_search)
-    menuItem.setOnActionExpandListener(object : MenuItem.OnActionExpandListener {
-      override fun onMenuItemActionExpand(item: MenuItem): Boolean {
-        return true
-      }
-
-      override fun onMenuItemActionCollapse(item: MenuItem): Boolean {
-        activity?.onBackPressed()
-        return false
-      }
-    })
-    menuItem.expandActionView()
-
-    val searchView = menuItem.actionView as SearchView
-    searchView.apply {
-      setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-        override fun onQueryTextSubmit(query: String): Boolean {
-          return onQueryTextChange(query)
-        }
-
-        override fun onQueryTextChange(newText: String): Boolean {
-          return true
-        }
-      })
-    }
-    RxBus.listen(DrawerOpenEvent::class.java).subscribe { searchView.clearFocus() }
   }
 
   override fun onAttach(context: Context?) {
