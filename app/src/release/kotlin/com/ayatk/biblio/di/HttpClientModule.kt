bc27e8c52311d250cfc811a78c05982cf54ba1aa
Aya Tokikaze <git@ayatk.com>
2017-09-14 16:37:19 +0900

    UAのセットをHttpClientModuleでするようにした

--- app/src/release/kotlin/com/ayatk/biblio/di/HttpClientModule.kt
+++ app/src/release/kotlin/com/ayatk/biblio/di/HttpClientModule.kt
@@ -5,10 +5,11 @@
 package com.ayatk.biblio.di
 
 import android.content.Context
+import com.ayatk.biblio.BuildConfig
 import dagger.Module
 import dagger.Provides
 import okhttp3.Cache
-import okhttp3.Interceptor
+import okhttp3.Interceptor.Chain
 import okhttp3.OkHttpClient
 import java.io.File
 import javax.inject.Singleton
@@ -19,15 +20,21 @@ class HttpClientModule {
   private val CACHE_FILE_NAME = "okhttp.cache"
   private val MAX_CACHE_SIZE = (4 * 1024 * 1024).toLong()
 
+  private val addUserAgentInterceptor = { chain: Chain ->
+    val builder = chain.request().newBuilder()
+    builder.addHeader("User-Agent", "BiblioAndroidApp/" + BuildConfig.VERSION_NAME)
+    chain.proceed(builder.build())
+  }
+
   @Singleton
   @Provides
-  fun provideHttpClient(context: Context, interceptor: Interceptor): OkHttpClient {
+  fun provideHttpClient(context: Context): OkHttpClient {
     val cacheDir = File(context.cacheDir, CACHE_FILE_NAME)
     val cache = Cache(cacheDir, MAX_CACHE_SIZE)
 
     val c = OkHttpClient
         .Builder()
-        .addInterceptor(interceptor)
+        .addInterceptor(addUserAgentInterceptor)
         .cache(cache)
 
     return c.build()
