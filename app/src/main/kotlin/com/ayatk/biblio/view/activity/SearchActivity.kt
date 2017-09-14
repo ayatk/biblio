8cb00a69005d2ed52532dd436ad51d97582d6ccd
Aya Tokikaze <git@ayatk.com>
2017-09-14 21:22:33 +0900

    SearchActivityを調整

---
+++ app/src/main/kotlin/com/ayatk/biblio/view/activity/SearchActivity.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2016-2017. Aya Tokikaze. All Rights Reserved.
+ */
+
+package com.ayatk.biblio.view.activity
+
+import android.content.Context
+import android.content.Intent
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import com.ayatk.biblio.R
+import com.ayatk.biblio.databinding.ActivitySearchBinding
+import com.ayatk.biblio.view.fragment.SearchFragment
+
+
+class SearchActivity : BaseActivity() {
+
+  private val binding: ActivitySearchBinding by lazy {
+    DataBindingUtil.setContentView<ActivitySearchBinding>(this, R.layout.activity_search)
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    overridePendingTransition(0, R.anim.activity_fade_exit)
+
+    initBackToolbar(binding.toolbar)
+    replaceFragment(SearchFragment.newInstance(), R.id.content_view)
+  }
+
+  override fun finish() {
+    super.finish()
+    overridePendingTransition(0, R.anim.activity_fade_exit)
+  }
+
+  override fun onBackPressed() {
+    finish()
+  }
+
+  companion object {
+    fun createIntent(context: Context): Intent = Intent(context, SearchActivity::class.java)
+  }
+}
