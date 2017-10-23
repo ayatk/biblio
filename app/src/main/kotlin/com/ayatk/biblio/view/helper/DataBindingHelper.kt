cfd0544e57168b3ebd03811dd4e28bb8aa9a71e2
ayatk <git@ayatk.com>
2017-10-24 01:27:59 +0900

    ユーザータグが表示されない問題を修正
    fixed #36
    BindingAdapterを使ってタグ追加の処理を共通化した

--- app/src/main/kotlin/com/ayatk/biblio/view/helper/DataBindingHelper.kt
+++ app/src/main/kotlin/com/ayatk/biblio/view/helper/DataBindingHelper.kt
@@ -4,12 +4,31 @@
 
 package com.ayatk.biblio.view.helper
 
+import android.content.Context
+import android.databinding.BindingAdapter
 import android.databinding.BindingConversion
 import android.graphics.drawable.ColorDrawable
+import android.view.LayoutInflater
+import android.widget.TextView
+import com.ayatk.biblio.R
+import com.google.android.flexbox.FlexboxLayout
 
 object DataBindingHelper {
 
   @JvmStatic
   @BindingConversion
   fun convertColorToDrawable(color: Int): ColorDrawable = ColorDrawable(color)
+
+  @JvmStatic
+  @BindingAdapter("tags")
+  fun FlexboxLayout.setTags(tags: List<String>) {
+    this.removeAllViews()
+    val inflater = this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+    tags.map {
+      val tagItem = inflater.inflate(R.layout.view_tag, null)
+      val textView = tagItem.findViewById<TextView>(R.id.tag)
+      textView.text = it
+      this.addView(textView)
+    }
+  }
 }
