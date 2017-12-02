4f2ef8aee77053074fc265b107cd272aa10eaf96
ayatk <git@ayatk.com>
2017-12-02 19:22:04 +0900

    Revert "Add MultiDex.install"
    This reverts commit 8108673885247248a7d824228e041682ab7c6737.

--- app/src/main/kotlin/com/ayatk/biblio/App.kt
+++ app/src/main/kotlin/com/ayatk/biblio/App.kt
@@ -4,8 +4,6 @@
 
 package com.ayatk.biblio
 
-import android.content.Context
-import android.support.multidex.MultiDex
 import com.ayatk.biblio.di.DaggerAppComponent
 import com.crashlytics.android.Crashlytics
 import com.squareup.leakcanary.LeakCanary
@@ -14,11 +12,6 @@ import dagger.android.support.DaggerApplication
 import io.fabric.sdk.android.Fabric
 
 open class App : DaggerApplication() {
-
-  override fun attachBaseContext(context: Context) {
-    super.attachBaseContext(context)
-    MultiDex.install(this)
-  }
 
   override fun onCreate() {
     super.onCreate()
